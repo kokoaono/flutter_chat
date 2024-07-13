ab42ac505f52368f5cc70007c0c08715358496f0
Koko Ono <nyswzt.a@gmail.com>
2024-07-13 15:12:40 +1200

    feat: add msg to db

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,19 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
+  late String? _currentUser;
+
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final _auth = FirebaseAuth.instance;
+  final TextEditingController chatTextController = TextEditingController();
 
-  void authChange() {
+  final _auth = FirebaseAuth.instance;
+  final _db = FirebaseFirestore.instance;
+
+  void getCurrentUser() {
+    /// This is listening to the change of auth state
     _auth.authStateChanges().listen((User? user) {
       if (user != null) {
-        final currentUser = user.email;
-        print(currentUser);
+        final loggedInUser = user.email;
+        print(loggedInUser);
       }
     });
+  }
+
+  void getMessages() async {
+    final messages = await _db.collection('messages').get().then((event) {
+      for (var doc in event.docs) {
+        print('${doc.id} => ${doc.data()}');
+      }
+    });
+  }
+
+  Future<void> addNewMsg(String text) async {
+    final message = <String, dynamic>{'text': text, 'sender': _currentUser};
+    await _db.collection('messages').add(message);
   }
 
   Future<void> registerNewUser(String email, String password) async {
@@ -24,10 +44,12 @@ class AuthController extends GetxController {
   }
 
   Future<void> loginUser(String email, String password) async {
-    await _auth.signInWithEmailAndPassword(
+    final logged = await _auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
+    _currentUser = logged.user?.email;
+    print('I am => ${logged.user?.email}');
   }
 
   void logOut() {
@@ -35,15 +57,15 @@ class AuthController extends GetxController {
   }
 
   @override
+  void onInit() {
+    super.onInit();
+    getCurrentUser();
+  }
+
+  @override
   void onClose() {
     super.onClose();
     emailController.dispose();
     passwordController.dispose();
-  }
-
-  @override
-  void onInit() {
-    super.onInit();
-    authChange();
   }
 }
